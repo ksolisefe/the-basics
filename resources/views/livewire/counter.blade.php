abd774ad9410ed0b80db0bc38a9af5f51c61fcfc
ksolis <ksolis@efe1963.com>
2025-09-29 12:01:03 -0400

    Basics livewire tutorial by the man himself

---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,5 @@
+<div>
+    Count: {{ $count }}
+    <br>
+    <button wire:click="increment(2)">+</button>
+</div>
---
+++ resources/views/livewire/hello-world.blade.php
@@ -0,0 +1,5 @@
+<div>
+    The current time is: {{ time() }}
+    <br>
+    <button wire:click="$refresh">Refresh Time</button>
+</div>
