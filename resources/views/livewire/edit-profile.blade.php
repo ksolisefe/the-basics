1c717b025a7857651600abbdf397296e155c8476
ksolis <ksolis@efe1963.com>
2025-10-01 16:36:44 -0400

    Accesibility things

--- resources/views/livewire/edit-profile.blade.php
+++ resources/views/livewire/edit-profile.blade.php
@@ -2,31 +2,37 @@
     <h1 class="mb-4 text-2xl text-slate-700 font-semibold">Update your profile...</h1>
 
     <form wire:submit="save" class="min-w-[30rem] flex flex-col gap-6 rounded-lg shadow p-6 bg-white/5">
-        <div class="flex flex-col gap-2">
+        <label class="flex flex-col gap-2">
             <h3 class="font-medium text-base">Username</h3>
 
             <input
+                
                 wire:model.live="form.username"
                 @class([
                     'px-3 py-2 rounded-lg text-black',
                     'border border-slate-300' => $errors->missing('form.username'),
                     'border-3 border-red-300' => $errors->has('form.username'),
                 ])
+                @error('form.username')
+                    aria-invalid="true"
+                    aria-description="{{ $message }}"
+                @enderror
             >
             @error('form.username')
-                <p class="text-sm text-red-500">{{ $message }}</p>            
+                <p class="text-sm text-red-500" aria-live="assertive">{{ $message }}</p>            
             @enderror
-        </div>
+        </label>
 
-        <div class="flex flex-col gap-2">
+        <label class="flex flex-col gap-2">
             <h3 class="font-medium text-base">Bio</h3>
 
             <textarea
+                aria-label="Bio"
                 wire:model="form.bio"
                 rows="4"
                 class="px-3 py-2 border border-slate-300 rounded-lg text-black"
                 ></textarea>
-        </div>
+        </label>
 
         <div class="flex">
             <button type="submit" class="relative w-full bg-blue-500 py-3 px-8 rounded-lg text-white font-medium disabled:cursor-not-allowed disabled:opacity-75">
@@ -51,6 +57,7 @@
         x-transition.opacity.out.duration.2000ms
         x-effect="if($wire.showSuccessIndicator) { setTimeout(() => $wire.set('showSuccessIndicator', false), 3000) }"
         class="flex justify-end space-x-2 mt-4"
+        aria-live="polite"
     >
         <span class="inline-block w-3 h-3 rounded-full bg-green-500 animate-pulse"></span>
         <span class="text-sm text-white-600">Profile is up to date</span>
